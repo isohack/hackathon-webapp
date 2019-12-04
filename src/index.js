60d01b796d50471c73794e3e52d73a2368498669
Shashank Sharma <shashank.sharma98@gmail.com>
2019-12-05 02:40:36 +0530

    Frontend: Sentry for React added + Fix req

--- src/index.js
+++ src/index.js
@@ -7,21 +7,27 @@ import * as serviceWorker from './serviceWorker';
 import LoginPage from './components/Dashboard/LoginPage';
 import PageNotFoundError from './components/ErrorPages/PageNotFoundError';
 import InternalServerError from './components/ErrorPages/InternalServerError';
+import * as Sentry from '@sentry/browser';
 import { createStore } from 'redux';
 import allReducer from './reducers';
 import { Provider } from 'react-redux';
 
 const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const sentry_token = process.env.REACT_APP_SENTRY_DSN;
+
+if (sentry_token) {
+  Sentry.init({dsn: sentry_token});
+}
 
 const routing = (
   <Provider store={store}>
     <Router>
-        <Switch>
-            <Route exact={true} path={"/"} component={App} />
-            <Route path={"/login"} component={LoginPage} />
-            <Route path={"/error/500"} component={InternalServerError} />
-            <Route component={PageNotFoundError} />
-        </Switch>
+      <Switch>
+        <Route exact={true} path={"/"} component={App} />
+        <Route path={"/login"} component={LoginPage} />
+        <Route path={"/error/500"} component={InternalServerError} />
+        <Route component={PageNotFoundError} />
+      </Switch>
     </Router>
   </Provider>
 );
