839d9d88f764af21d22e0c764037ae782139fcc7
Shashank Sharma <shashank.sharma98@gmail.com>
2019-12-16 22:48:56 +0530

    Fix travis

--- src/components/Utilities/ScrollUp.jsx
+++ src/components/Utilities/ScrollUp.jsx
@@ -18,6 +18,7 @@ class ScrollUp extends Component{
 		return(
 			<>
 			<div className="fixed-action-btn">
+				// eslint-disable-next-line react/jsx-no-target-blank
 				<a className="btn-floating red scroll-up-button scale-transition scale-out scale-in" 
 				id="scroll-up-button" onClick={()=>{ window.scrollTo(0, 0); }}>
 					<i className="material-icons scroll-up-button-icon">keyboard_arrow_up</i>
