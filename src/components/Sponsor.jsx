d5f24b1dfde4f28601dc205b9235fc3e2d04e1c5
Swarup Hegde <swaruphegde@gmail.com>
2019-12-19 22:56:13 +0530

    Frontend: Created sponsor section prototype (#78)
    * feat: added prizes section
    
    * fixed conflict in prizes
    
    * feat: revamped faq section
    
    * fix : modified FAQ section
    
    * fix: fixed sponsors
    
    * fix: fixed build issues in sponsor
    
    * final fix of sponsor prototype

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -1,45 +1,34 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../css/sponsor.css";
-import Sponsor_1 from "../img/sponsor_1.png";
-import Sponsor_2 from "../img/sponsor_2.jpeg";
-import Sponsor_3 from "../img/sponsor_3.png";
-import Sponsor_4 from "../img/sponsor_4.jpg";
-import Sponsor_dell from "../img/sponsor-dell.jpg";
-class Sponsor extends Component
-{
-    render()
-    {
-        return(
-            <div className="sponsor-back">
-                <h1 className="sponsor-header"> Sponsors</h1>
-                <div className="row">
-                    <div className="col s12">
-                        <img src={Sponsor_1} alt="sponsor" className="sponsor-img"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col s12">
-                        <img src={Sponsor_2} alt="sponsor" className="sponsor-img"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col s12">
-                        <img src={Sponsor_3} alt="sponsor" className="sponsor-img"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col s12">
-                        <img src={Sponsor_4} alt="sponsor" className="sponsor-img"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col s12">
-                        <img src={Sponsor_dell} alt="sponsor" className="sponsor-img"/>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+class Sponsor extends Component {
+  render() {
+    return (
+      <>
+        <div className="sponsorBg">
+          <h1 className="header-text text-gradient">Sponsors</h1>
+
+          <div className="row">
+            <a href="https://devfolio.co/">
+              <img
+                className="devfolio"
+                src="https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/spaces%2F-LX2wpUXI1eWMY46nkHR%2Favatar-rectangle.png?generation=1570189666128075&alt=media"
+                alt="devfolio"
+              />
+            </a>
+          </div>
+          <div className="row">
+            <a href="https://matic.network/">
+              <img
+                className="matic"
+                src="https://cryptologos.cc/logos/matic-network-matic-logo.png"
+                alt="matic"
+              />
+            </a>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Sponsor;
