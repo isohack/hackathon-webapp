5f4d38d26953bf9b3503941b0dc562e925347d8b
Shashank Sharma <shashank.sharma98@gmail.com>
2019-12-21 16:18:13 +0530

    Frontend: Footer Changed (#86) (#88)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Footer.css';
 import "../css/timeGradient.css";
+import logo from '../logo-favicon -android-chrome-512x512.png'
 
 class Footer extends Component {
   state = {};
@@ -11,14 +12,56 @@ class Footer extends Component {
 
   render() {
     return (
-      <div className={"footer-container"}>
-        <div className={"footer-ground"} >
+
+
+      <div class="container-fluid footer"   >
+        <div class="row footer-row">
+          <div class="col-12">
+            <a href={"/"} className=" footer-title">
+              <img className="footer-logo-img" src={logo} alt="NOT LOADED" />
+            </a>
+          </div>
         </div>
-        <div className={"footer-background-1"}>
+
+
+        <div class="row footer-row footer-info">
+          <div class="col-12">
+            <a href="mailto:isohack.atc@gmail.com" >isohack.atc@gmail.com</a>
+          </div>
+
+
+          <div class="col-12" align='center'>
+            Copyright © 2019 IsoHack
+          </div>
         </div>
-        <div onClick={() => {this.props.footerTestHandler()}} className={"footer-runner"}>
+
+
+        <div class="row footer-row justify-content-around footer-social-links">
+
+          <div class="col-sm-4 col-12">
+            <a href="https://www.github.com/"  >Github</a>
+          </div>
+
+          <div class="col-sm-4 col-12">
+            <a href="https://www.facebook.com/" >Facebook</a>
+          </div>
+
+          <div class="col-sm-4 col-12">
+            <a href="https://twitter.com/" >Twitter</a>
+          </div>
+
+          <div class="col-sm-4 col-12">
+            <a href="https://www.instagram.com/" >Instagram</a>
+          </div>
         </div>
       </div>
+
+
+
+
+
+
+
 
     );
   }
