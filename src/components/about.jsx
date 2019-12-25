5dad4632f1c7eac5f8e672a47912a4a4e1cef917
Swarup Hegde <swaruphegde@gmail.com>
2019-12-25 23:13:35 +0530

    Updated about section text (#116)

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,13 +8,17 @@ const About = () => (
   >
     <div className="row about-body">
       <div className="col s12 m6 l6">
-        <h3 className="about-header">An isolated mind sets fire to new ideas, a group of such minds is a bonfire</h3>
+        <h3 className="about-header">What is Isohack?</h3>
         <p
           className="white-text flow-text"
         >
-          <p>IsoHack is an effort at draining young minds looking for a challenge at solving problems with innovation and yes some programming too.</p>
-		  <p>Orgainzed by a team of devout individuals of Acropolis Technical Campus Indore, IsoHack promises to be your best experience at hacking.</p>
-		  <p>Pickup the challenge and get to be a part of something remarkable happening in the cleanest city and the heart of central India -INDORE!</p>
+          Isohack is a collection of like-minded people from various walks of life, attempting to solve a pertaining social issue, 
+          or to improve an existing solution from all over India. Isohack aims at providing a stage where enthusiasts can 
+          display their abilities and unlock their potential.<br/>
+          The 36-hour long hackathon will be embedded with various engrossing workshops, inspiring talks and other interactive sessions from experienced 
+          individuals from the industry. 
+          Pickup the challenge and get to be a part of something remarkable happening in the cleanest city and the heart of central India 
+
 		  <br/><br/>
         </p>
       </div>
