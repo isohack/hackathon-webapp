2551202cdb91280c6cb7cfe5ee238168ff4c8827
Shashank Sharma <shashank.sharma98@gmail.com>
2019-12-28 17:19:21 +0530

    Frontend: Fix bug

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import * as serviceWorker from './serviceWorker';
 import LoginPage from './components/Dashboard/LoginPage';
 import PageNotFoundError from './components/ErrorPages/PageNotFoundError';
 import InternalServerError from './components/ErrorPages/InternalServerError';
-import TestPage from "./components/TestPage";
 import CodeOfConductPage from './components/CodeOfConductPage';
 import * as Sentry from '@sentry/browser';
 import { createStore } from 'redux';
@@ -16,25 +15,26 @@ import { Provider } from 'react-redux';
 
 const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const sentry_token = process.env.REACT_APP_SENTRY_DSN;
-const safeURL = window.location.host !== 'www.isohack.in' && window.location.host !== 'localhost:3000' && window.location.host !== 'isohack.herokuapp.com';
+//const safeURL = window.location.host !== 'www.isohack.in' && window.location.host !== 'localhost:3000' && window.location.host !== 'isohack.herokuapp.com';
 
+//console.log(window.location.host);
 
 if (sentry_token) {
   Sentry.init({dsn: sentry_token});
 }
 
-const routing_unsafe = (
-  <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route
-          path='/'
-          render={(props) => <InternalServerError {...props} safeURL={safeURL} />}
-        />
-      </Switch>
-    </Router>
-  </Provider>
-);
+// const routing_unsafe = (
+//   <Provider store={store}>
+//     <Router>
+//       <Switch>
+//         <Route
+//           path='/'
+//           render={(props) => <InternalServerError {...props} safeURL={safeURL} />}
+//         />
+//       </Switch>
+//     </Router>
+//   </Provider>
+// );
 
 const routing_safe = (
   <Provider store={store}>
@@ -42,8 +42,7 @@ const routing_safe = (
       <Switch>
         <Route exact={true} path={"/"} component={App} />
         <Route path={"/login"} component={LoginPage} />
-        <Route path={"/test"} component={TestPage} />
-		<Route path={"/code-of-conduct/"} component={CodeOfConductPage} />
+		    <Route path={"/code-of-conduct/"} component={CodeOfConductPage} />
         <Route path={"/error/500"} component={InternalServerError} />
         <Route component={PageNotFoundError} />
       </Switch>
@@ -51,10 +50,10 @@ const routing_safe = (
   </Provider>
 );
 
-if (safeURL) {
-  ReactDOM.render(routing_unsafe, document.getElementById('root'));
-  serviceWorker.unregister();
-} else {
+// if (safeURL) {
+  // ReactDOM.render(routing_unsafe, document.getElementById('root'));
+  // serviceWorker.unregister();
+// } else {
   ReactDOM.render(routing_safe, document.getElementById('root'));
   serviceWorker.unregister();
-}
+// }
