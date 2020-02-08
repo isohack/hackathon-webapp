2a4a9846cf4ff99c16ffab40720483f2b4a6f455
Shashank Sharma <shashank.sharma98@gmail.com>
2020-02-08 22:25:27 +0530

    Fixing sponsor section

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "../css/sponsor.css";
 import DevfolioLogo from "../img/devfolio.png";
 import MaticLogo from "../img/matic.png";
+import GithubLogo from "../img/github-logo.png";
+import FoldLogo from "../img/fold-logo.png";
+import EBLogo from "../img/eb-logo.png";
+import SlackLogo from "../img/slack-logo.jpeg";
 import Pdf from '../Sponsors.pdf';
 
 class Sponsor extends Component {
@@ -10,24 +14,26 @@ class Sponsor extends Component {
       <>
         <div className="sponsorBg">
           <h1 className="sponsor-header-text text-gradient">Sponsors</h1>
-          <div className="row">
-            <a href="https://devfolio.co/">
-              <img
-                className="devfolio"
-                src={DevfolioLogo}
-                alt="devfolio"
-              />
-            </a>
-          </div>
-          <div className="row">
-            <a href="https://matic.network/">
-              <img
-                className="matic"
-                src={MaticLogo}
-                alt="matic"
-              />
-            </a>
-          </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className={"row center"}>
+                <div className={"col s12 m6 l4"}>
+                    <img className={"sponsor-logo-new"} src={GithubLogo} alt={"Github"} style={{height: "20vmin"}} />
+                </div>
+                <div className={"col s12 m6 l4"}>
+                    <img className={"sponsor-logo-new"} src={DevfolioLogo} alt={"Devfolio"} style={{height: "10vmin"}} />
+                </div>
+                <div className={"col s12 m6 l4"}>
+                    <img className={"sponsor-logo-new"} src={MaticLogo} alt={"Matic"} style={{height: "10vmin"}} />
+                </div>
+            </div>
+            <div className={"row center"}>
+                <div className={"col s12 m6 l4"}>
+                    <img className={"sponsor-logo-new"} src={FoldLogo} alt={"Fold"} style={{height: "10vmin"}} />
+                </div>
+            </div>
           <a href={Pdf} className="sponsor-us-button center white-text" target="_blank" rel="noopener noreferrer">Sponsor Us</a>
           {/*<button className="sponsor-us-button">*/}
             {/**/}
